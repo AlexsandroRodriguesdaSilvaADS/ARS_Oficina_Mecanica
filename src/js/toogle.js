fb4a7b64f8a1ae22a6373467349baed51018e551
alexsandro <autonomosslm@gmail.com>
2026-07-24 23:05:41 -0300

    atualização

--- src/js/toogle.js
+++ src/js/toogle.js
@@ -19,10 +19,8 @@ function toggleMenu4() {
 
 
 let show = true;
-
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
-
 // Menu em max-width 425px //
 menuToggle.addEventListener("click", () => {
 /*document.body.style.overflow = show ? "hidden" : "initial"*/
